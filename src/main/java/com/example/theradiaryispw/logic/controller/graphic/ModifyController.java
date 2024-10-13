94f5468c5e9fa633b1a89524c651f38e2170792b
tostfia <tostisofia09@libero.it>
2024-10-13 15:50:26 +0200

    Creato schermata account e modifica dati, iniziato parzialmente task

---
+++ src/main/java/com/example/theradiaryispw/logic/controller/graphic/ModifyController.java
@@ -0,0 +1,11 @@
+package com.example.theradiaryispw.logic.controller.graphic;
+
+import com.example.theradiaryispw.logic.otherClasses.other.Session;
+
+public class ModifyController extends CommonController{
+
+    public ModifyController(Session session) {
+        super(session);
+    }
+}
+
